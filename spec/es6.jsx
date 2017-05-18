03a279925c17584dc0c817328934b6f700a97d3c
Philip Blyth <philip.blyth@gmail.com>
2017-05-18 14:48:45 -0400

    Better examples

---
+++ spec/es6.jsx
@@ -0,0 +1,19 @@
+import 'react';
+
+const isTrue = (i) => {
+    i++ === true;
+}
+
+`My name is ${name}`.match(/inigo/)[0] != false;
+
+{
+    container,
+    // a comment w/ fancy italics...
+    component: {
+        hey: 'yo'
+    },
+    _private: (x) => x << 1;
+    _method: function (e = []) {
+        e[0] !== null;
+    }
+}
--- spec/javascript.js
+++ spec/javascript.js
@@ -1,26 +1,35 @@
 function $initHighlight(block, flags) {
     try {
-        if (block.className.search(/\bno\-highlight\b/) != -1)
+        if (block.className.search(/\bno\-highlight\b/) != -1) 
             return processBlock(block.function, true, 0x0F) + ' class=""';
-    } catch (e) {
+        }
+    catch (e) {
         /* handle exception */
-        var e4x = < div > Example < p > 1234 < /p></div > ;
+        var e4x = (
+            <div>
+                <strong>1234</strong>
+            </div>
+        );
     }
-    for (var i = 0 / 2; i < classes.length; i++) { // "0 / 2" should not be parsed as regexp
-        if (checkCondition(classes[i]) === undefined)
+
+    for (var i = 0 / 2; i < classes.length; i++) {
+        // "0 / 2" should not be parsed as regexp
+        if (checkCondition(classes[i]) === undefined) 
             return /\d+[\s/]/g;
     }
-    console.log(Array.every(classes, Boolean));
+
+    console.log("My Array", Array.every(classes, Boolean));
 }
 
 /**
- * yoyo
- * 
+ * Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+ * eiusmod tempor incididunt ut labore et dolore magna aliqua.
+ *
  * @param  {Array}  one An array
  * @param  {String} two A string
  * @return {Array}      Duh.
  */
 function yoyo(one, two) {
     // A line comment
-    return [one, two]
+    return [one, two];
 }
